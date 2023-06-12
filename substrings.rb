ab208d4f798953945e287ffd8cd96985b10d6db0
hroulston <h_roulston@live.com>
2023-06-12 15:52:44 -0700

    Create a method substring that takes a work and checks dictionary for word and substrings and adds those substring into a hash. Hash is then returned.

--- substrings.rb
+++ substrings.rb
@@ -4,8 +4,10 @@
 #in the original string and how many times it was found.
 #Next, make sure your method can handle multiple words:
 
-  dictionary = ["below","down","go","going","horn","how","howdy","it","i",
-                "low","own","part","partner","sit"]
+dictionary = ["how", "come", "be", "ever", "eve"]
 
 def substrings (word, list)
+  word_count = Hash.new(0)
+  list.each { |a| word_count[a] += 1 if word.include?(a)}
+  word_count
 end
