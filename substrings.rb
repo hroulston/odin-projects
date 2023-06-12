1c16b073fda1b3761279c728318ec6fd63e5a513
hroulston <h_roulston@live.com>
2023-06-12 14:16:43 -0700

    First commit.

---
+++ substrings.rb
@@ -0,0 +1,11 @@
+#Implement a method #substrings that takes a word as the first argument 
+#and then an array of valid substrings (your dictionary) as the second argument. 
+#It should return a hash listing each substring (case insensitive) that was found 
+#in the original string and how many times it was found.
+#Next, make sure your method can handle multiple words:
+
+  dictionary = ["below","down","go","going","horn","how","howdy","it","i",
+                "low","own","part","partner","sit"]
+
+def substrings (word, list)
+end
