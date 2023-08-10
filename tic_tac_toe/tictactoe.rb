177dce685d202b1994d68ab156d8fb46eb4d3514
hroulston <h_roulston@live.com>
2023-08-10 11:37:11 -0700

    Creates and displays game board.

---
+++ tic_tac_toe/tictactoe.rb
@@ -0,0 +1,8 @@
+def game_board
+    puts " #{1} " "|" " #{2} " "|" " #{3} "
+    puts separator = "-----------"
+    puts " #{4} " "|" " #{5} " "|" " #{6} "
+    puts separator
+    puts " #{7} " "|" " #{8} " "|" " #{9} "
+end
+
