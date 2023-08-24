2e8c222e3f270c5604693e49685603b008f41715
hroulston <h_roulston@live.com>
2023-08-24 11:35:54 -0700

    Works through debugging code. Can get the game to play but still need to adjust how board is displayed.

--- tic_tac_toe/tictactoe2.rb
+++ tic_tac_toe/tictactoe2.rb
@@ -1,44 +1,25 @@
-Module TicTacToe
+module TicTacToe
 
     WIN = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-    def print_slow string
-        string.each_char do |value|
-        puts value
-        sleep 0.06
-        end
-        puts ""
-    end
-
+    
     class Game
-        def initialize(player1, player2)
+        def initialize(player1 = Player, player2 = Player)
             @board = Array.new(9)
             @current_player_id = 0
             @players = [player1.new(self, "X"), player2.new(self, "O")]
             print_slow "#{which_player_name} goes first."
         end
         attr_reader :board, :current_player_id
-
-        def current_player
-            @players[@current_player]
-        end
-
-        def opposing_player_id
-            1 - @current_player_id
+        
+        def print_slow string
+            string.each_char do |value|
+                print value
+                sleep 0.06
+            end
+            puts ""
         end
         
-        def opposing_player
-            @players[opposing_player_id]
-        end
-
-        def which_player_name
-            opposing_player_id == 0 ? "Player 1" : "Player 2"
-        end
-
-        def switch_players!
-            @current_player_id = opposing_player_id
-        end
-
         def play
             loop do
                 place_marker(current_player)
@@ -57,6 +38,28 @@ Module TicTacToe
             end
         end
 
+        def current_player
+            @players[@current_player_id]
+        end
+
+        def opposing_player_id
+            1 - @current_player_id
+        end
+        
+        def opposing_player
+            @players[opposing_player_id]
+        end
+
+        def which_player_name
+            opposing_player_id == 0 ? "Player 2" : "Player 1"
+        end
+
+        def switch_players!
+            @current_player_id = opposing_player_id
+        end
+
+    
+
         def position_available
             (0..8).select {|position| @board[position].nil?}
             #looking at numbers 0-8 if the board index of that number is nil then
@@ -74,14 +77,17 @@ Module TicTacToe
         end
 
         def player_won?(player)
-            LINES.any? do |line|
+            WIN.any? do |line|
                 line.all? {|position| @board[position] == player.marker}
             end
         end
 
         def print_board
-            board_array = [1,2,3,4,5,6,7,8,9]
-            puts " #{board_array[0]} " "|" " #{board_array[1]} " "|" " #{board_array[2]} "
+            #Still working through how to display board properly
+            @board.each.with_index(1) do |item, index|
+                print index
+            end
+            puts " #{@board} " "|" " #{board_array[1]} " "|" " #{board_array[2]} "
             puts separator = "-----------"
             puts " #{board_array[3]} " "|" " #{board_array[4]} " "|" " #{board_array[5]} "
             puts separator
@@ -100,14 +106,14 @@ Module TicTacToe
             #makes the change immutable so another player cannot take this position by using bang.
             @game.print_board
             loop do
-                print_slow "Select your position number for marker #{marker}: "
-                selction = gets.to_i
+                @game.print_slow "Select your position number for marker #{marker}: "
+                selection = gets.to_i
                 return selection if @game.position_available.include?(selection)
-                print_slow "Position #{selection} is not available. Try again."
+                @game.print_slow "Position #{selection} is not available. Try again."
             end
         end
     end
 end
 
-two_players = [Player, Player]
-Game.new(two_players).play
+# two_players = [TicTacToe::Player, TicTacToe::Player]
+TicTacToe::Game.new.play
