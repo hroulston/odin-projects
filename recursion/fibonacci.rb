24f11b62ac3f6a9fc4d82cd649ff07757f40f930
hroulston <h_roulston@live.com>
2023-12-13 14:48:06 -0700

    Sets numbers to start sequence. Works on iteration, has not completed recursion version of method.

--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -1,3 +1,11 @@
 def fibs (number)
-    
+    num_arr = [0,1]
+    number.times do 
+        first_num = 0
+        next_num = 1
+        first_num = next_num
+        next_num = first_num + next_num
+    end
 end
+
+fibs(8)
