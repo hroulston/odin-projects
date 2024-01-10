d56618e4f0e7b8ee436d76977e40aaf675a3b0ba
hroulston <h_roulston@live.com>
2024-01-09 22:22:33 -0700

    Creates method for fibonacci sequence using recursion

--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -1,14 +1,27 @@
-def fibs (number)
-    num_arr = [0,1]
-    num1 = num_arr[0]
-    num2 = num_arr[1]
-    number.times do
-        new_num = num1 + num2
-        num_arr.push(new_num)
-        num1 = num2
-        num2 = new_num
-    end
-    puts num_arr
+# fibs using iteration
+# def fibs (number)
+#     num_arr = [0,1]
+#     num1 = num_arr[0]
+#     num2 = num_arr[1]
+#     number.times do
+#         new_num = num1 + num2
+#         num_arr.push(new_num)
+#         num1 = num2
+#         num2 = new_num
+#     end
+#     puts num_arr
+# end
+
+# fibs(12)
+
+# fibs using recursion
+
+def fibs_rec (number)
+   if number > 2
+    return (fibs_rec(number-1) + fibs_rec(number-2))
+   else
+    return 1
+   end
 end
 
-fibs(12)
+fibs_rec(8)
