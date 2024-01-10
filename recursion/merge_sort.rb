98ea926caff7be88f13375900d09f0f40fcd70ae
hroulston <h_roulston@live.com>
2024-01-09 22:26:36 -0700

    Creates merge_sort file and method

---
+++ recursion/merge_sort.rb
@@ -0,0 +1,7 @@
+# Build a method #merge_sort that takes in an array and returns a sorted array, using a recursive merge sort methodology. An input of [3, 2, 1, 13, 8, 5, 0, 1] should return [0, 1, 1, 2, 3, 5, 8, 13], and an input of [105, 79, 100, 110] should return [79, 100, 105, 110].
+# Tips:
+# Think about what the base case is and what behavior is happening again and again and can actually be delegated to someone else (e.g. that same method!).
+# It may be helpful to check out the background videos again if you don’t quite understand what should be going on.
+
+merge_sort(arr)
+end
